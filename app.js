8c4ad1052382300a4c78f1b8f9c501cb1d3b90cb
Danica Rachel Lim <drachel.lim@gmail.com>
2023-09-07 17:39:20 +0800

    up

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ hiddenElements.forEach((el) => observer.observe(el));
 /* 
 START reference: https://www.youtube.com/watch?v=TpwpAYi-p2w 
 */
-let greenRing = document.querySelector('.mouse-ring');
+// let greenRing = document.querySelector('.mouse-ring');
 
-window.addEventListener('mousemove', (e) => {
-  greenRing.style.left = e.clientX + 'px';
-  greenRing.style.top = e.clientY + 'px';
-});
+// window.addEventListener('mousemove', (e) => {
+//   greenRing.style.left = e.clientX + 'px';
+//   greenRing.style.top = e.clientY + 'px';
+// });
 
 /* END */
 
@@ -33,27 +33,27 @@ window.addEventListener('mousemove', (e) => {
 /* For switching between color modes and toggle states.
   Saves user preferences in local storage. */
 // call input with class
-const toggle = document.querySelector('.toggle-switch');
+// const toggle = document.querySelector('.toggle-switch');
 
-// call current mode saved in local storage
-const currentMode = localStorage.getItem('mode');
+// // call current mode saved in local storage
+// const currentMode = localStorage.getItem('mode');
 
-// checks if current mode is light
-if (currentMode == 'light') {
-  document.body.classList.add('light-mode'); // sets CSS color values to light mode
-  toggle.checked = true; // sets toggle state to true
-}
+// // checks if current mode is light
+// if (currentMode == 'light') {
+//   document.body.classList.add('light-mode'); // sets CSS color values to light mode
+//   toggle.checked = true; // sets toggle state to true
+// }
 
-// checks if toggle switch is clicked
-toggle.addEventListener('click', () => {
-  document.body.classList.toggle('light-mode'); // add 'light-mode' class to body
+// // checks if toggle switch is clicked
+// toggle.addEventListener('click', () => {
+//   document.body.classList.toggle('light-mode'); // add 'light-mode' class to body
 
-  let mode = 'dark'; // declares mode
-  if (document.body.classList.contains('light-mode')) { // checks if body has 'light-mode' class
-    mode = 'light'; // sets mode to light
-  }
-  localStorage.setItem('mode', mode); // stores mode in local storage
-});
+//   let mode = 'dark'; // declares mode
+//   if (document.body.classList.contains('light-mode')) { // checks if body has 'light-mode' class
+//     mode = 'light'; // sets mode to light
+//   }
+//   localStorage.setItem('mode', mode); // stores mode in local storage
+// });
 
 /* END */
 
